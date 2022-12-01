51712e9110d10b9220fbfb72ad62312a75cebb34
Burundukov Denis <burundukov.d@yandex.ru>
2022-12-01 15:11:06 +0500

    Task 38 completed

---
+++ Task 36/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void SummNum(int [] arr)
+        {
+            int summ = 0;
+            for (int i = 1; i < arr.Length; i += 2)
+            {
+                arr[i] = new Random().Next(100, 999);
+                if (arr[i] % 2 != 0) 
+                summ += arr[i];
+
+            }
+            Console.WriteLine($"i  find  sum  of the odd numbers in the array, his equals is: {sum}");
+        }
+
+SummNum();
---
+++ Task 38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.0 7.2 22.7 2.1 78.3] -> 76.2
+void ArrDif()
+        {
+            Random rnd = new Random();
+            int[] arr = new int[20];
+            int min = 100, max = 0;
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(100);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max) max = arr[i];
+                if (arr[i] < min) min = arr[i];
+              
+            }
+            int difference = max - min;
+            Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {difference} ");
+        }
+ ArrDif();
